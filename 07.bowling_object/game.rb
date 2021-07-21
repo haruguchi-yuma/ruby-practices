79ab01946b798c581779d605014611c794abc358
yuma <ikeoku.y@gmail.com>
2021-07-21 15:06:42 +0900

    rubocopによる指摘を修正

--- 07.bowling_object/game.rb
+++ 07.bowling_object/game.rb
@@ -1,13 +1,16 @@
+# frozen_string_literal: true
+
 require_relative './frame'
 
 class Game
   STRIKE = 10
+
   def initialize(score)
     @marks = score.split(',')
   end
 
   def score
-    frames = divide_into_frames(@marks).map { |frame| Frame.new(*frame)}
+    frames = divide_into_frames(@marks).map { |frame| Frame.new(*frame) }
     frames.each_with_index.sum do |frame, i|
       if last_frame?(i)
         frame.score
@@ -21,34 +24,35 @@ class Game
     end
   end
 
+  private
+
   def divide_into_frames(marks)
     frames = []
     10.times do |n|
-      if n == 9
-        frames << marks
-      elsif marks.first == 'X'
-        frames << [marks.shift]
-      else
-        frames << marks.shift(2)
-      end
+      frames << if n == 9
+                  marks
+                elsif marks.first == 'X'
+                  [marks.shift]
+                else
+                  marks.shift(2)
+                end
     end
     frames
   end
 
-  def added_strike_point(frames, i)
-    if frames[i+1].strike? 
-      STRIKE * 2 + frames[i+2].first_shot.score
+  def added_strike_point(frames, index)
+    if frames[index + 1].strike?
+      STRIKE * 2 + frames[index + 2].first_shot.score
     else
-      STRIKE + frames[i + 1].score - frames[i + 1].third_shot.score
+      STRIKE + frames[index + 1].score - frames[index + 1].third_shot.score
     end
   end
 
-  def added_spare_point(frames, i)
-    frames[i].score + frames[i+1].first_shot.score
+  def added_spare_point(frames, index)
+    frames[index].score + frames[index + 1].first_shot.score
   end
 
-  def last_frame?(i)
-    i == 9
+  def last_frame?(index)
+    index == 9
   end
 end
-
