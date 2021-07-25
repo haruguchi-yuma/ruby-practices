460d36daefaaf1d4c1345733c733ef26400d2aca
yuma <ikeoku.y@gmail.com>
2021-07-25 17:01:12 +0900

    テストを追加した

---
+++ 08.ls_object/test/ls_test.rb
@@ -0,0 +1,60 @@
+require 'pathname'
+require 'minitest/autorun'
+require_relative '../ls'
+
+class LsTest < Minitest::Test
+  TARGET_PATHNAME = Pathname('test/sample-app')
+
+  def test_run_ls_short_format
+    ls = Ls.new(TARGET_PATHNAME,**{ detail: false, reverse: false, dot_match: false })
+    expected = <<~TEXT.chomp
+    Gemfile           config            postcss.config.js
+    Gemfile.lock      config.ru         public
+    README.md         db                storage
+    Rakefile          lib               test
+    app               log               tmp
+    babel.config.js   node_modules      vendor
+    bin               package.json      yarn.lock
+    TEXT
+    assert_equal expected, ls.run_ls
+  end
+
+  def test_run_ls_long_format
+    ls = Ls.new(TARGET_PATHNAME,**{ detail: true, reverse: false, dot_match: false })
+    expected = `ls -l #{TARGET_PATHNAME}`.chomp
+    assert_equal expected, long_format_files.run_ls_long_format
+  end
+
+  def test_run_ls_reverse
+    ls = Ls.new(TARGET_PATHNAME,**{ detail: false, reverse: true, dot_match: false })
+    expected = <<~TEXT.chomp
+    yarn.lock         package.json      bin
+    vendor            node_modules      babel.config.js
+    tmp               log               app
+    test              lib               Rakefile
+    storage           db                README.md
+    public            config.ru         Gemfile.lock
+    postcss.config.js config            Gemfile
+    TEXT
+    assert_equal expected, ls.run_ls
+  end
+
+  def test_run_ls_dot_match
+    ls = Ls.new(TARGET_PATHNAME,**{ detail: false, reverse: false, dot_match: true })
+    expected = <<~TEXT.chomp
+      .                 README.md         config.ru         postcss.config.js
+      ..                Rakefile          db                public
+      .browserslistrc   app               lib               test
+      .ruby-version     babel.config.js   log               tmp
+      Gemfile           bin               node_modules      vendor
+      Gemfile.lock      config            package.json      yarn.lock
+    TEXT
+    assert_equal expected, ls.run_ls
+  end
+
+  def test_run_ls_all_options
+    ls = Ls.new(TARGET_PATHNAME,**{ detail: true, reverse: true, dot_match: true })
+    expected = `ls -arl #{TARGET_PATHNAME}`.chomp
+    assert_equal expected, ls.run_ls
+  end
+end
